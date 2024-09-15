5d0aba721a77916fd98bf35f31db57912c945c60
Javi7512 <javi7518524225@gmail.com>
2024-09-15 23:40:19 +0530

    Add files via upload
    Simple calculator

---
+++ Simple Calculator.py
@@ -0,0 +1,30 @@
+def calculator():
+    print("Welcome to Simple Calculator")
+    try:
+        num1=float(input("Enter the first number : "))
+        num2=float(input("Enter the second number : "))
+    except ValueError:
+        print("Invalid input ! Please enter numerical value.")
+        return
+    print("Choose an operation : ")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (+)")
+    operation = input("Enter the operation (1/2/3/4): ")
+    if operation == "1":
+        result = num1 + num2
+        print(f"{num1}+{num2}={result}")
+    elif operation == "2":
+        result = num1 - num2
+        print(f"{num1}-{num2}={result}")
+    elif operation == "3":
+        result = num1 * num2
+        print(f"{num1}*{num2}={result}")
+    elif operation == "4":
+        if num2!=0:
+            result = num1 / num2
+            print(f"{num1}/{num2}={result}")
+        else:
+            print("Invalid operation ! Please choose 1,2,3,4.")
+calculator()
